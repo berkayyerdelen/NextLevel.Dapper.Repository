4cc9442b63cd512c0a4312656563a089d986e289
Berkay Yerdelen <berkayyerdelen@gmail.com>
2020-09-06 23:18:09 +0300

    Add project files.

---
+++ NextLevel.Dapper.Repository/Service/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace NextLevel.Dapper.Repository.Service.Repository
+{
+    public interface IRepository<TEntity, in TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(string command);
+        ValueTask<TEntity> GetByIdAsync(string command, TKey id);
+        Task AddAsync(string command,TEntity entity);
+        Task UpdateAsync(string command,TEntity entity, TKey id);
+        Task RemoveAsync(string command,TKey id);
+        Task<bool> IsInDbAsync(string command,TKey id);
+    }
+}
---
+++ NextLevel.Dapper.Repository/Service/Repository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace NextLevel.Dapper.Repository.Service.Repository
+{
+    public class Repository<TEntity, TKey> : BaseRepository, IRepository<TEntity, TKey>
+    {
+        public Repository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public async Task AddAsync(string command, TEntity entity)
+        {
+            await WithConnection(async conn =>
+                await conn.ExecuteAsync(command, entity));
+
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(string command)
+        {
+            return await WithConnection(async conn =>
+                await conn.QueryAsync<TEntity>(command));
+        }
+
+        public async ValueTask<TEntity> GetByIdAsync(string command, TKey id)
+        {
+            return await WithConnection(async conn =>
+                await conn.QueryFirstOrDefaultAsync<TEntity>(command, new { Id = id }));
+
+        }
+
+        public async Task<bool> IsInDbAsync(string command, TKey id)
+        {
+            var result = await WithConnection(async conn =>
+                 await conn.ExecuteAsync(command, new { Id = id }));
+            return result == 1;
+        }
+
+        public async Task RemoveAsync(string command, TKey id)
+        {
+            await WithConnection(async conn =>
+                await conn.ExecuteAsync(command, new { Id = id }));
+        }
+
+        public async Task UpdateAsync(string command, TEntity entity, TKey id)
+        {
+            await WithConnection(async conn =>
+                await conn.ExecuteAsync(command, entity));
+        }
+    }
+}
